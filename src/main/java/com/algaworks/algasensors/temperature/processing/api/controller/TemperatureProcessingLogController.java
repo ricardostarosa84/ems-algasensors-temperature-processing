6e31cb4d3cd0e92a586b39270239dc942338965c
ricardostarosa84 <ricardostarosa84@gmail.com>
2026-01-03 18:11:12 -0300

    part1: sensor micro-service sync

--- src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingLogController.java
+++ src/main/java/com/algaworks/algasensors/temperature/processing/api/controller/TemperatureProcessingLogController.java
@@ -1,6 +1,5 @@
 package com.algaworks.algasensors.temperature.processing.api.controller;
 
-
 import com.algaworks.algasensors.temperature.processing.api.model.TemperatureLogOutput;
 import com.algaworks.algasensors.temperature.processing.common.IdGenerator;
 import io.hypersistence.tsid.TSID;
